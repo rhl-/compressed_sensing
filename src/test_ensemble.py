a3564ac02d0b3bfa6389a748019580be91ce57a4
Ryan H. Lewis <me@ryanlewis.net>
2015-02-09 14:36:47 -0800

    unit tests commit.

--- src/test_ensemble.py
+++ src/test_ensemble.py
@@ -6,9 +6,19 @@ import gen_ensemble as ensemble
 
 
 class ensemble_test(unittest.TestCase):
+	def is_perm_format(self,A):
+		nnz = np.count_nonzero( A)
+ 		self.assertEquals( nnz, self.m*self.n*self.n)
+		for matrix in A:
+			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
+			for row in np.reshape(matrix, (self.n,self.n)):
+				self.assertEquals( np.count_nonzero( row), 1)
+			for column in np.reshape(matrix, (self.n,self.n)).T:
+				self.assertEquals( np.count_nonzero( column), 1)
 
 	def get_sample(self,t1, t2=common.TARGET_TYPES.HPSD):
 		return ensemble.getEnsembleSample(self.m, self.n, t1, t2)
+
 	def setUp(self):
 		self.m = 128
 		self.n = 128
@@ -16,101 +26,66 @@ class ensemble_test(unittest.TestCase):
 	def test_entry(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.ENTRY)
 		nnz = np.count_nonzero(A)
- 		self.assertTrue(nnz == self.m)
+ 		self.assertEquals(nnz, self.m)
 		for matrix in A:
-			self.assertTrue(np.count_nonzero(matrix == 1))
+			self.assertEquals(np.count_nonzero(matrix), 1)
 		
 	def test_perm(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.PERM)
-		nnz = np.count_nonzero( A)
- 		self.assertTrue( nnz == self.m*self.n*self.n)
-		for matrix in A:
-			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
-			for row in np.reshape(matrix, (n,n)):
-				self.assertTrue( np.count_nonzero( row) == 1)
-			for column in np.reshape(matrix, (n,n)).T:
-				self.assertTrue( np.count_nonzero( column) == 1)
+		self.is_perm_format( A)
 
 	def test_rsperm(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.RSPERM)
-		nnz = np.count_nonzero( A)
- 		self.assertTrue( nnz == self.m*self.n*self.n)
-		for matrix in A:
-			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
-			for row in np.reshape(matrix, (n,n)):
-				self.assertTrue( np.count_nonzero( row) == 1)
-			for column in np.reshape(matrix, (n,n)).T:
-				self.assertTrue( np.count_nonzero( column) == 1)
+		self.is_perm_format( A)
 		B = n*np.multiply(A,A);	
-		self.assertTrue( abs(sum(sum(B)) - np.count_nonzero( B)) < 1e-4)
+		self.assertAlmostEqual( sum(sum(B)), np.count_nonzero( B))
 
 	def test_csperm(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.CSPERM)
-		nnz = np.count_nonzero( A)
- 		self.assertTrue( nnz == self.m*self.n*self.n)
-		for matrix in A:
-			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
-			for row in np.reshape(matrix, (n,n)):
-				self.assertTrue( np.count_nonzero( row) == 1)
-			for column in np.reshape(matrix, (n,n)).T:
-				self.assertTrue( np.count_nonzero( column) == 1)
+		self.is_perm_format( A)
 		B = np.multiply(A,A)
 		B = np.multiply(B,B)
 		B = (n**2)*B
-		self.assertTrue( abs(sum(sum(B)) - np.count_nonzero( B)) < 1e-4)
+		self.assertAlmostEqual( sum(sum(B)), np.count_nonzero( B))
 
 	def test_rgperm(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.RGPERM)
-		nnz = np.count_nonzero( A)
- 		self.assertTrue( nnz == self.m*self.n*self.n)
-		for matrix in A:
-			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
-			for row in np.reshape(matrix, (n,n)):
-				self.assertTrue( np.count_nonzero( row) == 1)
-			for column in np.reshape(matrix, (n,n)).T:
-				self.assertTrue( np.count_nonzero( column) == 1)
+		self.is_perm_format( A)
 		B = n*np.multiply(A,A)
-		self.assertTrue( abs(sum(sum(B)) - np.count_nonzero( B)) < 1e-4)
+		self.assertAlmostEqual( sum(sum(B)), np.count_nonzero( B))
 
 	def test_cgperm(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.CGPERM)
-		nnz = np.count_nonzero( A)
- 		self.assertTrue( nnz == self.m*self.n*self.n)
-		for matrix in A:
-			self.assertTrue( np.count_nonzero( matrix) == self.n**2)
-			for row in np.reshape(matrix, (self.n,self.n)):
-				self.assertTrue( np.count_nonzero( row) == 1)
-			for column in np.reshape(matrix, (self.n,self.n)).T:
-				self.assertTrue( np.count_nonzero( column) == 1)
+		self.is_perm_format( A)
 		B = np.multiply(A,A)
 		B = np.multiply(B,B)
 		B = (n**2)*B
-		self.assertTrue( abs(sum(sum(B)) - np.count_nonzero( B)) < 1e-4)
+		self.assertAlmostEqual( sum(sum(B)), np.count_nonzero( B))
 		B = np.nonzeroflatten( A);
-		self.assertTrue( np.mean( B) < 1e-3)
-		self.assertTrue( abs(np.mean( numpy.multiplies(B,B)) - 1.0/n) < 1e-3)
+		self.assertAlmostEqual( np.mean( B), 0)
+		self.assertAlmostEqual( np.mean( np.multiply( abs(B), abs(B))), 1.0/(self.n**2))
 
 	def test_rdirac(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.RDIRAC)
 		for row in A:
-			self.assertTrue(np.linalg.norm( row) == 1)
+			self.assertEqual(np.linalg.norm( row), 1)
 
 	def test_cdirac(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.CDIRAC)
 		for row in A:
-			self.assertTrue(np.linalg.norm( row) == 1)
+			self.assertEqual(np.linalg.norm( row),1)
 	
 	def test_rgauss(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.RGAUSS)
 		B = A.flatten();
-		self.assertTrue( abs(np.mean( B)) < 1e-3)
-		self.assertTrue( abs(np.mean( np.multiply( abs(B), abs(B))) - 1.0/(self.n**2)) < 1e-3)
+		self.assertAlmostEqual( np.mean( B), 0)
+		self.assertAlmostEqual( np.mean( np.multiply( abs(B), abs(B))), 1.0/(self.n**2))
 	
 	def test_cgauss(self):
 		A = self.get_sample(common.ENSEMBLE_TYPES.CGAUSS)
 		B = A.flatten();
-		self.assertTrue( abs(np.mean( B)) < 1e-3)
-		self.assertTrue( abs(np.mean( np.multiply( abs(B), abs(B))) - 1.0/(self.n**2)) < 1e-3)
+		self.assertAlmostEqual( np.mean( B), 0)
+		self.assertAlmostEqual( np.mean( np.multiply( abs(B), abs(B))), 1.0/(self.n**2))
 
 if __name__ == '__main__':
     unittest.main()
