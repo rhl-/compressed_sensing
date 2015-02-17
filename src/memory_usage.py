bdfc48a8fe893e7490ccd951aebba036afa7c879
Victor Minden <victorminden@gmail.com>
2015-02-17 11:32:07 -0800

    new timing

--- src/memory_usage.py
+++ src/memory_usage.py
@@ -1,5 +1,10 @@
+
+import resource
+
 def memory_usage():
-    # return the memory usage in GB
-    import psutil, os
-    process = psutil.Process(os.getpid())
-    return process.get_memory_info()[0] / float(10 ** 9)
+    # # return the memory usage in GB
+    # import psutil, os
+    # process = psutil.Process(os.getpid())
+    # return process.get_memory_info()[0] / float(10 ** 9)
+
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(10 ** 9)
