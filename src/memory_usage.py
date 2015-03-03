851258995899107c036c5703741818c8f5a9a564
Ryan H. Lewis <me@ryanlewis.net>
2015-03-02 19:59:35 -0800

    memory usage fixed...

--- src/memory_usage.py
+++ src/memory_usage.py
@@ -1,10 +1,10 @@
 
-import resource
-
+import resource, platform
 def memory_usage():
     # # return the memory usage in GB
     # import psutil, os
     # process = psutil.Process(os.getpid())
     # return process.get_memory_info()[0] / float(10 ** 9)
-
-    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(10 ** 9)
+    if(platform.system() == "Darwin"):
+    	return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(10 ** 9)
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / float(10 ** 6)
